e9f325807a0ff683c98d8c7bc918c3c3367806dc
Đỗ Thị Ngân <nganthido483@gmail.com>
2024-10-24 17:56:02 +0700

    first commit

---
+++ Week1/24-10/bai1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+double factorial(int n) {
+    double result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int main(){
+    double epsilon;
+    // int n = 1;
+    double e = 1.0;
+    cin >> epsilon;
+    if(0<epsilon && epsilon<1){
+        for(int n = 1; ;n++){
+            e+=1/factorial(n);
+            if(1/factorial(n)<epsilon)
+                break;
+        }
+        cout << e;
+    }
+    else{
+        cout << "Yeu cau nhap 0< epsilon <1";
+    }
+}
---
+++ Week1/bai2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	long long i;
+	cin >> i;
+	if(i % 2 == 0){
+		cout << "CHAN";
+	}
+	else{
+		cout <<"LE";
+	}
+	return 0;
+}
